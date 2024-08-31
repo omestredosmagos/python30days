2a5e545d64bde7b6eaadab99b5c10501a7a88c21
[nathan alves] <[nathan.alves.r2n@gmail.com]>
2024-08-31 15:18:35 -0300

    update: exercise completed

---
+++ EX1D1/python30daysEX1D1.py
@@ -0,0 +1,46 @@
+operand1 = 3
+operand2 = 4
+name = "Nathan"
+familyName = "Silva"
+country = "Brazil"
+
+print("#### Part 1 ####\n")
+
+# Example of Addition
+print("Addition of ", operand1, " and ",operand2, " >> Result = ", operand1+operand2)
+
+# Example of Subtraction
+print("Subtraction of ", operand1, " and ",operand2, " >> Result = ", operand1-operand2)
+
+# Example of Multiplication
+print("Multiplication of ", operand1, " and ",operand2, " >> Result = ", operand1*operand2)
+
+# Example of Modulus
+print("Modulus of ", operand1, " and ",operand2, " >> Result = ", operand1%operand2)
+
+# Example of Division
+print("Division of ", operand1, " and ",operand2, " >> Result = ", operand1/operand2)
+
+# Example of Exponential
+print("Exponential of ", operand1, " and ",operand2, " >> Result = ", operand1**operand2)
+
+# Example of Floor Division Operator
+print("Floor division of ", operand1, " and ",operand2, " >> Result = ", operand1//operand2)
+
+print("#### Part 2####\n")
+
+print("My name is "+name)
+print("My family name is "+familyName)
+print("I am from "+country)
+print("I am enjoying 30 days of Python")
+
+print("#### Part 3####\n")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Nathan', 'Duda', 'Python']))
+print(type(name))
+print(type(familyName))
+print(type(country))
