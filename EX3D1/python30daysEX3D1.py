8d01d553d69666aa161b6d48c3b2ca391cd5736b
[nathan alves] <[nathan.alves.r2n@gmail.com]>
2024-08-31 17:40:11 -0300

    update: execise completed

--- EX3D1/python30daysEX3D1.py
+++ EX3D1/python30daysEX3D1.py
@@ -1,1 +1,19 @@
+print('#### Part 1 ####\n')
 
+print('Number Integer: ', 6)
+print('Number Float: ', 3.14)
+print('Number Complex: ', 1+1j)
+print('String: ', 'Duda é linda')
+print('Boolean: ', True)
+print('List: ', ['pão', 'queijo', 'presunto', 'café'])
+print('Tuple: ', ('python', 'java', 'c', 'javascript'))
+print('Set: ', {15, 1, 50, 6})
+print('Dictionary: ', {'nome': 'duarda', 'idade': 24, 'qualidade': 'inteligente'})
+
+print('\n#### Part 2 ####\n')
+
+dataPoints1 = (2,3)
+dataPoints2 = (10,8)
+euclidianDistance = (((dataPoints1[0] - dataPoints2[0])**2)+((dataPoints1[1] - dataPoints2[1])**2))**(1/2)
+
+print('Euclidian Distance for the data points: ', dataPoints1, ' and ', dataPoints2, ' is = ', euclidianDistance)
