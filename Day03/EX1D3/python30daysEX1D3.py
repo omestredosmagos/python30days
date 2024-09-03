3255c31fd06b79f16cf86bb9685f1a6423e00aa0
[nathan alves] <[nathan.alves.r2n@gmail.com]>
2024-09-03 18:14:55 -0300

    add: saving the checkpoint of the exercise 1 from day 3

---
+++ Day03/EX1D3/python30daysEX1D3.py
@@ -0,0 +1,18 @@
+age = 28
+height = 1.83
+complex_number = 1+2j
+
+# Calculating the triagle area
+base = input('Insert the triangle\'s base value: ')
+height = input('Insert the triangle\'s height: ')
+triangle_area = 0.5*float(base)*float(height)
+
+print('\nThe area of the triangle is ', triangle_area)
+
+# Calculating the triagle perimeter
+side1 = input('\nInsert the first side of the triangle: ')
+side2 = input('Insert the second side of the triangle: ')
+side3 = input('Insert the third side of the triangle: ')
+perimeter = side1+side2+side3
+print('\nThe perimeter of the triangle is ', perimeter)
+
